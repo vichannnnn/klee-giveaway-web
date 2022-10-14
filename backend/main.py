656cf1de4953d93262ae32c89d0619ec4d8f0013
hima <eugenell@uuzu.com>
2022-10-14 20:52:31 +0800

    update path

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ class NotAuthenticatedException(Exception):
 with open("authentication.yaml", "r", encoding="utf8") as stream:
     yaml_data = yaml.safe_load(stream)
 
-redirect_uri = "https://dev.himaaa.xyz/callback"
+redirect_uri = "https://dev.himaaa.xyz/api/callback"
 discord_client = DiscordOAuthClient(yaml_data['CLIENT_ID'], yaml_data['CLIENT_SECRET'],
                                     redirect_uri, scopes=('identify', 'guilds'))
 intents = discord.Intents.default()
@@ -60,7 +60,7 @@ async def validate_session(request: Request, url: str):
 
 
 def init_app():
-    app = FastAPI()
+    app = FastAPI(root_path='/api')
 
     @app.exception_handler(NotAuthenticatedException)
     def auth_exception_handler(request: Request, exc: NotAuthenticatedException):
